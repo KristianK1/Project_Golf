b2e6cf67f2030a85bebaedfba8a47460c8f4aa06
Kristian Klišković <kikihd11@gmail.com>
2020-07-26 00:07:44 +0200

    stvoreno funkcije.h

---
+++ skica_za_Arduino/funkcije.h
@@ -0,0 +1,19 @@
+int ulaz1=A1;
+int ulaz2=A2;
+int ulaz3=A3;
+
+int input_time=0;
+
+int istek_minute=3;
+
+bool citanje_ulaza(){
+  if(digitalRead(ulaz1)||digitalRead(ulaz2)||digitalRead(ulaz3)){
+    input_time=millis();
+    return HIGH;
+  }
+  else if(input_time-millis()<(istek_minute*60*1000)){
+      return HIGH;
+  }
+  else
+    return LOW;
+}
--- skica_za_Arduino/skica_za_Arduino.ino
+++ skica_za_Arduino/skica_za_Arduino.ino
@@ -1,29 +1,11 @@
 #include <SoftwareSerial.h>
 #include"klase.h"
+#include"funkcije.h"
+
 SoftwareSerial GSMSerial(10, 11);
 SoftwareSerial GPSSerial(4,3);
 
-int ulaz1=A1;
-int ulaz2=A2;
-int ulaz3=A3;
-
-int input_time=0;
-int istek_minute=3;
 Koordinate trenutne_koor, prosle_koor, nove_koor;
-
-Koordinate polje[10];
-
-bool citanje_ulaza(){
-  if(digitalRead(ulaz1)||digitalRead(ulaz2)||digitalRead(ulaz3)){
-    input_time=millis();
-    return HIGH;
-  }
-  else if(input_time-millis()<(istek_minute*60*1000)){
-      return HIGH;
-  }
-  else
-    return LOW;
-}
 
 void setup() {
   // put your setup code here, to run once:
