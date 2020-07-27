e188dc9c9a2a75ff941707c1fc56efd0d9cb07b6
Kristian Klišković <kikihd11@gmail.com>
2020-07-27 21:42:11 +0200

    zapoceta classa koor_1D

--- skica_za_Arduino/GPS.h
+++ skica_za_Arduino/GPS.h
@@ -7,45 +7,50 @@ double udaljenost(Koordinate &K, double X, double Y){
   return sqrt(pow(K.getX()-X,2)+pow(K.getY()-Y,2));
 }
 
-
 bool GPS_mjerenje(Koordinate* mjer, Koordinate *mjesta){
+  
   mjer->set(18,45);  //primjer, ovdje treba ubaciti mjerenje
+  bool vjerodostojnost=1;
 
-  //provjera mjerenja
-  if(mjer->getX()<=180&&mjer->getX()>=-180&&mjer->getY()<90&&mjer->getY()>-90){
-    //racunje srednje vrijednosti zadnji broj_lokacija+1 mjerenja
-    int br=0;
-    double Xavg=0, Yavg=0, sigma=0;
-    int i;
-
-    Xavg+=mjer->getX();
-    Yavg+=mjer->getY();
-    br++;
-    for(i=0;i<broj_lokacija;i++){
-      if(mjesta[i].getX()<=180&&mjesta[i].getX()>=-180&&mjesta[i].getY()<90&&mjesta[i].getY()>-90){
-        br++;
-        Xavg+=mjesta[i].getX();
-        Yavg+=mjesta[i].getY();
+  
+  if(vjerodostojnost==1){
+    //provjera mjerenja
+    if(mjer->getX()<=180&&mjer->getX()>=-180&&mjer->getY()<90&&mjer->getY()>-90){
+      //racunje srednje vrijednosti zadnji broj_lokacija+1 mjerenja
+      int br=0;
+      double Xavg=0, Yavg=0, sigma=0;
+      int i;
+  
+      Xavg+=mjer->getX();
+      Yavg+=mjer->getY();
+      br++;
+      for(i=0;i<broj_lokacija;i++){
+        if(mjesta[i].getX()<=180&&mjesta[i].getX()>=-180&&mjesta[i].getY()<90&&mjesta[i].getY()>-90){
+          br++;
+          Xavg+=mjesta[i].getX();
+          Yavg+=mjesta[i].getY();
+        }
       }
-    }
-    Xavg/=br;
-    Yavg/=br;
-
-    br=0;
-
-    sigma+=udaljenost(*mjer, Xavg, Yavg);
-    br++;
-    //racunanje devijacije zadnjih broj_lokacija+1 mjerenja
-    for(i=0;i<broj_lokacija;i++){
-      if(mjesta[i].getX()<=180&&mjesta[i].getX()>=-180&&mjesta[i].getY()<90&&mjesta[i].getY()>-90){
-        br++;
-        sigma+=udaljenost(mjesta[i], Xavg,Yavg);
+      Xavg/=br;
+      Yavg/=br;
+  
+      br=0;
+  
+      sigma+=udaljenost(*mjer, Xavg, Yavg);
+      br++;
+      //racunanje devijacije zadnjih broj_lokacija+1 mjerenja
+      for(i=0;i<broj_lokacija;i++){
+        if(mjesta[i].getX()<=180&&mjesta[i].getX()>=-180&&mjesta[i].getY()<90&&mjesta[i].getY()>-90){
+          br++;
+          sigma+=udaljenost(mjesta[i], Xavg,Yavg);
+        }
       }
+      sigma/=br;
+      sigma=sqrt(sigma);
+      return sigma;
     }
-    sigma/=br;
-    sigma=sqrt(sigma);
+    else Serial.println("Primljena totalno kriva koordinata (nije na zemlji)");
   }
-  else Serial.println("Primljena totalno kriva koordinata (nije na zemlji)");
 }
 
 
@@ -58,5 +63,9 @@ bool GPS_update(Koordinate *mjesta){
     for(i=broj_lokacija-1; i>0;i--){
       mjesta[i]=mjesta[i-1];
     }
+    mjesta[0]=*novo_mjerenje;
+  }
+  else if(valid==LOW){
+    Serial.println("Nevaljalo mjerenje zbog nekog razloga");
   }
 }
--- skica_za_Arduino/klase.h
+++ skica_za_Arduino/klase.h
@@ -1,3 +1,38 @@
+class koor_1D{
+  friend koor_1D operator-(koor_1D&, koor_1D&);
+  private:
+    int stupanj;
+    int minuta;
+    int sekunda;
+    double decimala_sekunde;
+  public:
+    koor_1D(): stupanj(0), minuta(0), sekunda(0), decimala_sekunde(0.0){
+      
+    }
+    koor_1D(int mStupnjevi, int mMinute, int mSekunde, double mDecimala): stupanj(mStupnjevi), minuta(mMinute), sekunda(mSekunde), decimala_sekunde(mDecimala){
+      
+    }
+    int get_stupanj(){ return stupanj;}
+    int get_minuta(){ return minuta;}
+    int get_sekunda(){ return sekunda;}
+    double get_decimala(){ return decimala_sekunde;};
+
+    void set_stupanj(int new_stupanj){ stupanj=new_stupanj;}
+    void set_minuta(int new_minuta){ minuta=new_minuta;}
+    void set_sekunda(int new_sekunda){ sekunda=new_sekunda;}
+    void set_decimala(double new_decimala){ decimala_sekunde=new_decimala;};
+};
+
+koor_1D operator-(koor_1D &lhs, koor_1D &rhs){
+  koor_1D ret;
+  ret.stupanj=lhs.stupanj-rhs.stupanj;
+  ret.minuta=lhs.minuta-rhs.minuta;
+  ret.sekunda=lhs.sekunda-rhs.minuta;
+  ret.decimala_sekunde=lhs.decimala_sekunde-rhs.decimala_sekunde;;
+}
+
+
+
 class Koordinate{
   friend bool operator!=(Koordinate &lhs, Koordinate &rhs);
   private:
@@ -29,4 +64,9 @@ bool operator!=(Koordinate &lhs, Koordinate &rhs){
     return LOW;
   }
   return HIGH;
+}
+
+Koordinate& operator-(Koordinate &lhs, Koordinate &rhs){
+  
+  
 }
