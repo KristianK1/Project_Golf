ccc3ace66dfee274ad77168ed271f7f74c4c0ef2
Kristian Klišković <kikihd11@gmail.com>
2020-12-31 00:24:38 +0100

    Update SIM800L_http.ino

--- SIM800L_http/SIM800L_http.ino
+++ SIM800L_http/SIM800L_http.ino
@@ -1,6 +1,6 @@
 #include <SoftwareSerial.h>
 
-SoftwareSerial mySerial(10, 11); // RX, TX
+SoftwareSerial mySerial(4,5); // RX, TX
 
 int slanje(SoftwareSerial serial, String out, String expect){
   serial.begin(9600);
@@ -15,12 +15,13 @@ int slanje(SoftwareSerial serial, String out, String expect){
     if(c!=10&&c!=13) recive+=(char)c;
     //Serial.println((char)c);
   }
-  //Serial.println("OJ");
+  Serial.println(recive);
+  
   if(recive==expect) return 1;
   return 0;
 }
 
-int HTTP(String link){
+/*int HTTP(String link){
   while(mySerial.available()) mySerial.read();
   Serial.println("jedan");
   slanje(mySerial, "AT","OK");
@@ -45,14 +46,70 @@ int HTTP(String link){
   Serial.println("osam");
   slanje(mySerial, "AT+HTTPREAD","");
   Serial.println("devet");
-  slanje(mySerial, "AT+HTTPTERM","OK");  
+  slanje(mySerial, "AT+HTTPTERM","OK");
+    
+}*/
+
+int HTTP(String link){
+  while(mySerial.available()) mySerial.read();
+  Serial.println("jedan");
+  slanje(mySerial, "AT","OK");
+  delay(1000);
+  
+  Serial.println("dva");
+  slanje(mySerial, "AT+CREG?","OK");
+  delay(1000);
+  
+  Serial.println("dva i po");
+  
+  slanje(mySerial, "AT+CSTT=\"TM\",\"\",\"\"","OK");
+  delay(1000);
+  
+  Serial.println("tri");
+  slanje(mySerial, "AT+SAPBR=2,1","OK");
+  delay(1000);
+  
+  Serial.println("cetiri");
+  slanje(mySerial, "AT+SAPBR=1,1","OK");
+  delay(1000);
+  
+  Serial.println("pet");
+  slanje(mySerial, "AT+HTTPINIT","OK");
+  delay(1000);
+  
+  Serial.println("sest");
+  String link_AT;
+  link_AT+="AT+HTTPPARA=\"URL\",\"";
+  link_AT+=link;
+  link_AT+="\"";
+  slanje(mySerial,link_AT,"");
+  delay(1000);
+  
+  Serial.println("sedam");
+  slanje(mySerial, "AT+HTTPPARA=\"CID\",1","OK");
+  delay(1000);
+  
+  Serial.println("sedam i po");
+  slanje(mySerial, "AT+HTTPACTION=0","OK");
+  delay(1000);
+  
+  Serial.println("osam");
+  slanje(mySerial, "AT+HTTPREAD","");
+  delay(1000);
+  
+  Serial.println("devet");
+  slanje(mySerial, "AT+HTTPTERM","OK");
+  delay(1000);
+    
 }
+
+
 
 void setup() {
   // Open serial communications and wait for port to open:
   Serial.begin(9600);
   mySerial.begin(9600);
-  HTTP("http://api.thingspeak.com/update?api_key=PY57W7KMYYBE2MDP&field1=120");
+  HTTP("http://api.thingspeak.com/update?api_key=PY57W7KMYYBE2MDP&field1=22");
 }
 
 
